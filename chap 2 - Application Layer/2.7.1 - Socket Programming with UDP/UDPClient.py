5a328df55694f891898ad914846ae408b27063c7
oraclown <owen.burton13@gmail.com>
2022-03-19 08:58:01 -0400

    Add UDP client & server example

---
+++ chap 2 - Application Layer/2.7.1 - Socket Programming with UDP/UDPClient.py
@@ -0,0 +1,13 @@
+from socket import *
+
+server_name = "hostname"
+server_port = 12000
+client_socket = socket(AF_INET, SOCK_DGRAM)
+
+message = input("Input lowercase sentence:")
+
+client_socket.sendto(message.encode(), (server_name, server_port))
+modified_message, server_address = client_socket.recvfrom(2048)
+
+print(modified_message.decode())
+client_socket.close()
